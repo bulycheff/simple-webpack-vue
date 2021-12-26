7a6286661eeddacc6d6832ac3a22cfbcea0416ef
Sergey Bulychev <bulycheff@ya.ru>
2021-12-26 09:39:38 +0500

    initial

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <input
+        v-model="message"
+        type="text">
+    <h2 class="message">{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Universe',
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-size: 18px;
+  font-family: 'Roboto', sans-serif;
+  color: blue;
+}
+</style>
